a89fcebd37283676c54506a260fe4088e6e1c8e6
3254c <chikara8460@icloud.com>
2023-10-30 15:35:35 +0900

    pancreas tutolial

--- tutolial/pancreas.py
+++ tutolial/pancreas.py
@@ -1,5 +1,5 @@
 import scvelo as scv
-import workflow
+import DeepKINET
 
 adata = scv.datasets.pancreas()
 scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
@@ -8,4 +8,4 @@ raw_adata = scv.datasets.pancreas()
 adata.layers['spliced'] = raw_adata[:, adata.var_names].layers['spliced']
 adata.layers['unspliced'] = raw_adata[:, adata.var_names].layers['unspliced']
 color = 'clusters'
-adata, vicdyf_exp = workflow.estimate_kinetics(adata, color = color)
+adata, vicdyf_exp = DeepKINET.workflow.estimate_kinetics(adata, color = color)
