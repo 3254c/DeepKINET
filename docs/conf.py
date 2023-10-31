46ee513ccbb9d7a52b8622af89170fdecbfdffd4
3254c <chikara8460@icloud.com>
2023-10-31 13:41:26 +0900

    extensions in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@ author = 'Chikara Mizukoshi'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
